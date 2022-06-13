b949cff490afe977a469ac722d7072cd9c5bc2cb
aavicii <94585649+aavicii@users.noreply.github.com>
2022-06-13 20:45:44 +0900

    Update Snake.java

--- src/com/kh/git/animal/Snake.java
+++ src/com/kh/git/animal/Snake.java
@@ -6,6 +6,7 @@ public class Snake {
    System.out.println("Snake run~");
      System.out.println("Snake run~");
   }
-  public void eat(){
-  System.out.println("Snake가 먹습니다"}
+ public void run(){
+   System.out.println("Snake eat~");
+  }
 }
